9695174c720a3713c83ddc00de139c2d893635ba
Aleksey <m100@list.ru>
2020-07-05 21:41:28 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/* jshint -W097 */
+"use strict";
+
+// alert('Hello');
+
+// const resoult = confirm('are you here?');
+
+// const answer = prompt('zzz?', '');
+// alert(typeof(answer));
+
+const answers = [];
+
+answers[0] = prompt('1?', '');
+answers[1] = prompt('2?', '');
+answers[2] = prompt('3?', '');
+
+document.write(answers);
