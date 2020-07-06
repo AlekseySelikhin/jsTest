ad72aa40b74e73cdcd85cf83b7521ffb9589d14c
Aleksey <m100@list.ru>
2020-07-06 17:25:12 +0500

    lesson 015

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,20 @@
+/* jshint -W097 */
+"use strict";
+
+//общение с пользователем
+//alert('');
+//promt('','');
+//confirm('');
+
 //тернарная операция - усеченная запись if/else
-const num = 50;
-(num == 50) ? console.log('отработало если') : console.log('отработало иаче');
+let num = 50;
+(num == 50) ? console.log('отработало если') : 
+                console.log('отработало иаче');
 
 //интерполяция (только если обратная косая ковычеа)
 console.log(`test ${num} test ${num}`);
+
+//условия
 
 //обычный if
 if (num == 49) {
@@ -34,4 +45,27 @@ switch (num) {
     default:
         console.log('=');
         break;    
+}
+
+//вопросы пользоватлю
+// const qwestion = promt('как чего?', '');
+
+//циклы
+while (num<55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num--;
+}
+while (num > 50);
+
+for (let i = 1; i < 8; i++) {
+    if (i == 6) {
+        // break;
+        continue;
+    }
+    console.log(i);
 }
--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
 /* jshint -W097 */
 "use strict";
 
-let numberOfFilms;
-
-numberOfFilms = prompt('Сколько фильмов ты смотрел?', '3')
+const numberOfFilms = +prompt('Сколько фильмов ты смотрел?', '3');
 
 const personalMovieDB = {
-    count: +numberOfFilms,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     private: false
 };
 
-let oneMovie = prompt('Назови один из последнийх фильмов', '');
-let movieScore = prompt('На что его оценишь?', '');
+//вариант 1
+for (let i = 0; i < numberOfFilms; i++)
+{
+    let a = prompt('Назови один из последнийх фильмов', '');
+    let b = prompt('На что его оценишь?', '');
+    if ( a != null && b != null && a != '' && b != '' && a.length <= 50 && b.length <= 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
 
-personalMovieDB.movies[oneMovie] = movieScore;
+//вариант 2
+let inc = numberOfFilms;
+while (inc > 0) {
+    console.log('работает_1');
+    inc--;
+}
+
+//вариант 3
+inc = numberOfFilms;
+do {
+    console.log('работает_2');
+    inc--;
+}
+while (inc > 0)
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотренно достаточно мало фильмов');
+} else if (personalMovieDB.count < 30) {
+    console.log ('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Поизошла ошибка');
+}
